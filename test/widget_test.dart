8bc43d136eace26c2f8c0525a84810ae593a6b76
willsonwill <willsonch.will@gmail.com>
2022-07-01 13:43:19 -0400

    Readme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:peces_app_yolov5/main.dart';
+import 'package:flutter_yolov5_pytorch/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
